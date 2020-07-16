15d1e5f7d79437e653af925eccb190ad7628f166
Sonechko777 <64007767+Sonechko777@users.noreply.github.com>
2020-07-16 23:44:32 +0300

    lv
    lv

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Monloco/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+    import Monloco from './components/Monloco.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+    components:{
+      Monloco
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
